902ed0d21bad28dfe9e32b12ffc81556e365b9f5
dvyrj <dvyrjchauhan@gmail.com>
2022-04-05 14:53:02 -0700

    client home styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,50 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Profile from './pages/profile';
 import ErrorPage from "./pages/error";
+import './styles/styles.css'
+
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home </Link>
-        <Link to="/login">Login </Link>
-        <Link to="/register">Register </Link>
-        <Link to="/example">Example </Link>
-      </nav>
+      <nav className="navigation">
+      <a href="/" className="brand-name">
+        QuickEats
+      </a>
+      <button className="hamburger">
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className="navigation-menu">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/register">Register</a>
+          </li>
+          <li>
+            <a href="/example">Example</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/home" element={<Home/>} />
