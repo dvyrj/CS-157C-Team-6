8bd1d52c45b47183ba89fb45df3e1c8b9f887a58
Tom Mach <tom.m.mach@gmail.com>
2022-04-24 00:28:37 -0700

    login page is pretty

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import '../styles/login.css'
 
 // Login
 function Login() {
     return (
         <>
-            <div>
-                <h1>Login</h1>
-            </div>
-            <form action="http://localhost:3001/loginAccount" method="GET">
-                <div><label> Email: <input type="text" name="email" /> </label></div>
-                <div><label> Password: <input type="text" name="password" /> </label></div>
-                <input type="submit" value="Submit" />
-            </form>
+            <html className='login_js'>
+            <body>
+                <div className='login_box'>
+                    <form className='login_form' action="http://localhost:3001/loginAccount" method="GET">
+                        <h1>Login</h1>
+                        <div className='email'><label> <input className='login_input' type="text" name="email" placeholder="Email"/> </label></div>
+                        <div className='password'><label> <input className='password_input' type="text" name="password" placeholder="Password" /> </label></div>
+                        <input className='submit_input' type="submit" value="Submit"/>
+                    </form>
+                </div>
+            </body>
+            </html>
         </>
     );
 }
