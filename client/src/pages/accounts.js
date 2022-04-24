a90fbae07530063afa4078cf5e06f983cb1f86cc
Tom Mach <tom.m.mach@gmail.com>
2022-04-24 15:14:59 -0700

    accounts done

--- client/src/pages/accounts.js
+++ client/src/pages/accounts.js
@@ -1,7 +1,7 @@
 
 // Admin: Display all users
 import React, { useEffect, useState } from 'react';
-import '../styles/styles.css'
+import '../styles/accounts.css'
 
 function Accounts() {
     const [backendData, setBackendData] = useState([{}]);
@@ -19,30 +19,30 @@ function Accounts() {
     return (
         <>
             <html className='account_js'>
-                <h1>Accounts</h1>
-                <div>
-                    <table>
+                <div className='account_box'>
+                    <h1 className='account_title'>Accounts</h1>
+                    <table className='account_table'>
                         <tr>
-                            <th>Account ID</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Account Type</th>
-                            <th>Email</th>
-                            <th>Password</th>
-                            <th>Member Since</th>
+                            <th className='account_id_th'>Account ID</th>
+                            <th className='first_name_th'>First Name</th>
+                            <th className='last_name_th'>Last Name</th>
+                            <th className='account_type_th'>Account Type</th>
+                            <th className='email_th'>Email</th>
+                            <th className='password_th'>Password</th>
+                            <th className='member_since_th'>Member Since</th>
                         </tr>
                         {
                             (typeof backendData.data === 'undefined') ? 
                             ( <p>Loading...</p> ) : (  backendData.data.map(
                                 (data, i) => (
                                     <tr key={i}>
-                                        <td>{data.account_id}</td>
-                                        <td>{data.first_name}</td>
-                                        <td>{data.last_name}</td>
-                                        <td>{data.account_type}</td>
-                                        <td>{data.email}</td>
-                                        <td>{data.password}</td>
-                                        <td>{data.date_added}</td>
+                                        <td className='account_id_td'>{data.account_id}</td>
+                                        <td className='first_name_td'>{data.first_name}</td>
+                                        <td className='last_name_td'>{data.last_name}</td>
+                                        <td className='account_type_td'>{data.account_type}</td>
+                                        <td className='email_td'>{data.email}</td>
+                                        <td className='password_td'>{data.password}</td>
+                                        <td className='member_since_td'>{data.date_added}</td>
                                     </tr>)
                                 )
                             )
