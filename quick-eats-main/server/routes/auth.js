20a72c2c058c8fd5355fddb307c3d65cc7fac07e
Tarun-Mourya <37710858+Tarun-Mourya@users.noreply.github.com>
2022-05-06 14:24:00 -0700

    Update auth.js

--- quick-eats-main/server/routes/auth.js
+++ quick-eats-main/server/routes/auth.js
@@ -1,1 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const CassandraClient = require('../databaseConn.js');
 
+const connection = require('../resources.js');
+
+router.post('/api/authenticate_emailid', async (req, res) => {
+
+  const credentials = req.body;
+  console.log('Fetching Details from DB');
+  const fetch_user = `SELECT * FROM users where user_emailId = ? ALLOW FILTERING`;
+  await CassandraClient.execute(fetch_user, [credentials.emailId],async function (error, results) {
+    if(error) {
+      console.log(error);
+    }
+    else {
+    console.log(results);
+    let returnData = [];
+      for (let i = 0; i < results.rowLength; i++) {
+        returnData.push(results.rows[i]);
+      }
+    results = returnData;
+    console.log(JSON.stringify(results));
+    if (results.length > 1) {
+      console.log("Multiple Entries exists, PLease Delete one user");
+      res.writeHead(301, {
+        'Content-Type': 'text/plain'
+      });
+      res.send("Duplicate Entries Found, Please Contact the System Admin");
+    }
+
+    else if (results.length === 0) {
+      console.log("User Doesn't exist");
+      res.writeHead(400, 'User Does not Exist', {
+        'Content-Type': 'text/plain'
+      });
+      res.end();
+    }
+
+    else if (results.length === 1) {
+      console.log('Successfully Retrieved the User');
+      if(results[0].user_pwd === credentials.pwd){
+        res.writeHead(200,results[0].restaurant_owner,{
+          'Content-Type': 'text/plain'
+        });
+      }
+      else{
+        res.writeHead(400,'INVALID CREDENTIALS',{
+          'Content-Type': 'text/plain'
+        });
+      }
+      res.end();
+    }
+
+    else if (error) {
+      console.log('Error Occured While Fetching the Data ' + error);
+      res.writeHead(400, 'Contact System Admin', {
+        'Content-Type': 'text/plain',
+      });
+      res.end();
+    }
+
+    else {
+      console.log('Unknown Fault Occured While Fetching the User');
+      res.writeHead(500, 'Contact System Admin', {
+        'Content-Type': 'text/plain',
+      });
+      res.end();
+    }
+  }
+  });
+});
+
+module.exports = router;
